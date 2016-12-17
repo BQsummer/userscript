de8171cf1bc20f81f46e73b1c46c42a0eedc1aad
BQsummer <bqsummer@gmail.com>
2016-12-18 02:21:46 +0800

    修改autoGetYuwan.js获取不到dom2后报错问题; 增加DouyuShowAllFollow.js

---
+++ DouyuShowAllFollow.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         DouyuShowAllFollow
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  显示所有关注的斗鱼主播列表，默认是只显示5个
+// @author       BQsummer
+// @match        https://www.douyu.com/*
+// @exclude      https://www.douyu.com/room/my_follow
+// @grant        none
+// @require      //cdn.bootcss.com/jquery/3.1.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //div.o-follow  ul.f-list
+    $("div.o-follow").mouseover(function(){
+        //$("ul.f-list").mouseover(function(){
+        $.ajax({
+            type: "GET",
+            url: "https://www.douyu.com/room/my_follow",
+            success: function(data){
+                //讲获取到的已关注主播信息添加到document中
+                var body_reg = /<div id="live-list-content"[^>]*>([\s\S]*)<\/div>/;
+                var result = body_reg.exec(data)[1];
+                $("body").append("<div id='ajaxCon' class='mySel' style='display: none;'>" + result+ "</div>");
+                //删除原主播列表
+                $("ul.f-list").children().remove();
+                //填充主播列表
+                if(!$("div.o-follow").hasClass("bq-change")) {
+                    $("div.o-follow").addClass("bq-change");
+                    var $titles = $("li[data-is-on='1']");
+                    var title;
+                    var href;
+                    var player;
+                    var num;
+                    $titles.each(function(index) {
+                        //console.log(index + ": " + $(this).children("a").children("div").children("div").children("h3").attr("title"));
+                        //console.log(index + ": " + $(this).attr("data-rid"));
+                        //console.log(index + ": " + $(this).children("a").children("div").children("p").children("span[class~='dy-name']").text());
+                        //console.log(index + ": " + $(this).children("a").children("div").children("p").children("span[class~='dy-num']").text());
+                        title = $(this).children("a").children("div").children("div").children("h3").attr("title");
+                        href = "/" + $(this).attr("data-rid");
+                        player =  $(this).children("a").children("div").children("p").children("span[class~='dy-name']").text();
+                        num = $(this).children("a").children("div").children("p").children("span[class~='dy-num']").text();
+                        $("ul.f-list").append("<li><p><a href='" + href + "' target='_blank'>" + title + "</a></p><span><a href='" + href + "' class='head-ico1'>已播　　分钟</a><a href='" + href + "' class='head-ico2'>" + player + "</a><a href='" + href + "' class='head-ico3'>" + num + "</a></span></li>");
+                    });
+                    $("div.f-pop").css({'height' : '375px', 'width' : '365', 'overflow-y' :'auto'});
+                }
+            }
+        });
+    });
+    $("div.f-pop").mouseleave(function(){
+        //删除body后添加的主播信息
+        $(".mySel").remove();
+        $('div[style="display: none; position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; cursor: move; opacity: 0; background: rgb(255, 255, 255);"]').remove();
+        //移除bq-change
+        $("div.o-follow").removeClass("bq-change");
+    });
+})();
--- autoGetYuwan.user.js
+++ autoGetYuwan.user.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name         autoGetYuwan
 // @namespace    http://tampermonkey.net/
 // @version      0.1
@@ -6,56 +6,56 @@
 // @author       BQsummer
 // @match        https://www.douyu.com/*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var date;
-    var hour;
-    var min;
-    var sec;
     var thread1;
     var thread2;
+    var dom1;
+    var dom2;
+    var interval = 150000;
     function search(interval) {
+        dom1 = document.getElementsByClassName("peck-cdn");
+        dom2 = document.getElementById("right_col_peck");
         console.log("function search:");
         if(thread2 !== undefined) {
             window.clearInterval(thread2);
             console.log("clear thread2");
         }
         var whethersearch  = window.setInterval(function() {
-            date = new Date();
-            hour = date.getHours();
-            min = date.getMinutes();
-            sec = date.getSeconds();
-            console.log("检测是否有鱼丸" + "  " + hour + ":" + min + ":" + sec);
-            var dom2 = document.getElementById("right_col_peck");
-            if(dom2.style.display == "block") {
-                thread2 = douyuClick();
+            console.log("检测是否有鱼丸");
+            dom2 = document.getElementById("right_col_peck");
+            if(!(dom2 === null || dom2 === undefined)) {
+                if(!(dom2.style === undefined || dom2.style === null)) {
+                    if(dom2.style.display == "block") {
+                        thread2 = douyuClick();
+                    }
+                }
             }
         }, interval);
         return whethersearch;
     }
 
     function douyuClick() {
+        dom1 = document.getElementsByClassName("peck-cdn");
+        dom2 = document.getElementById("right_col_peck");
         console.log("function douyuClick:");
         if(thread1 !== undefined) {
             window.clearInterval(thread1);
             console.log("clear thread1");
         }
-        var whetherStop = 1;
-        var time = 0;
-        var dom1 = document.getElementsByClassName("peck-cdn");
-        var dom2 = document.getElementById("right_col_peck");
         var thread = window.setInterval(function() {
             dom1[0].click();
             if(dom2.style.display == "none") {
-                thread1 = search();
+                thread1 = search(interval);
             }
         }, 500);
         return thread;
     }
 
-    thread1 = search(10000);
+    thread1 = search(interval);
 
 
 })();
